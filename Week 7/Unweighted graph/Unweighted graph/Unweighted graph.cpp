52bd29b372ba7e7ac42339daa0f49d972161ffa0
Fabienkfoury <fabienkfoury@hotmail.fr>
2016-11-29 17:51:15 +0000

    DFS and BFS DONE , now lets finish INORDER

--- Week 7/Unweighted graph/Unweighted graph/Unweighted graph.cpp
+++ Week 7/Unweighted graph/Unweighted graph/Unweighted graph.cpp
@@ -22,7 +22,7 @@ public:
 	void addEdges(Vertice *V1, Vertice *V2)
 	{
 		edges[V1->value][V2->value] = true;
-		//edges[V2->value][V1->value] = true;
+		edges[V2->value][V1->value] = true;
 	}
 
 	void InitializationOfEdge()
@@ -46,12 +46,11 @@ public:
 class MyGraph 
 {
 public :
-		int n;								// number of vertices ?
+		int n;								
 		list<Vertice*> listVertices;
 
-		MyGraph(int n/*, bool edges[15][15]*/)
+		MyGraph(int n)
 		{
-			//this->edges = edges;
 			this->n = n;
 		}
 		
@@ -71,43 +70,29 @@ public :
 
 
 
-list <int> DFS(MyGraph *G, Vertice *V)			
+list <int> DFS(MyGraph *G, Vertice *V)			//DEPTH FIRST SEARCH
 {	
-	// i think that the queue shoud be an array or a list , dont know how to create one
-	
-	//int n = G->n;							// number of vertices of the graph
-								// Creation of a queue thanks to "#include <queue>"	
 	stack <int> S;
 	list <int> visited;
 	S.push(V->value);	
-	//visited.push_front(V->value);
-	
-	// ADD the value of the vertice in the queue
 
-	//bool visited [15];						// Array to know if a vertice has been visited
-	//visited[V->value] = true;				// first vertice is visted
-	
-	//vector<int> wasVisited;
-	
-											// Q.front() = next thing in the queue
-											// Q.back() = the last think in the queue
 	while (!S.empty())
 	{
 		int u = S.top();
 		S.pop();
-		
-		//std::cout <<  u << std::endl;		// Display u
 
 		if (find(visited.begin(), visited.end(), u) == visited.end())			// if u has not been visited
 		{
-			visited.push_back(u);				// append u to visited
+			visited.emplace_back(u);				// append u to visited
 			//for (int i = 0; i < G->n; i++)		// For each element of edge matrix
 			//{
-				for (int j = 0; j < G->n; j++)
+			for (int j = G->n; j >0 ; j--)
 				{
-					if (V->edges[u][j])			// if there is and edge between vertice i and j
+					if (V->edges[u][j] && !pushed)			// we start from j = n because its a stack, so it will alway start from the top
+															// the probleme was that if a vertice has a least 2 edges, the second edge will come on the top of the stack
+															// instead of the first one
 					{
-						S.push(j);				// I have to push the value of the vertice wich is edged , push J, value of the vertice
+						S.push(j);							// I have to push the value of the vertice wich is edged , push J, value of the vertice
 					}
 				}
 			//}
@@ -116,7 +101,7 @@ list <int> DFS(MyGraph *G, Vertice *V)
 		
 		
 	}
-	//return wasVisited;
+
 	return visited;
 }
 
@@ -124,30 +109,21 @@ list <int> BFS(MyGraph *G, Vertice *V)			//BREADTH FIRST SEARCH
 {
 	queue <int> Q;
 	list<int> visited;
-	
-	
-	
-	//int n = G->n;
-	
-	//vector<int> wasVisited;
-	//visited[V->value] = true;		// Automaticaly visited
 	Q.push(V->value);
 
 	while (!Q.empty())
 	{
-		//std::cout << Q.top();
 		int u = Q.front();
 		 Q.pop();
 		 
-
 		 if (find(visited.begin(), visited.end(), u) == visited.end())
 		 {
-			 visited.emplace_back(u);
-			 for (int i = 0; i < G->n; i++)		
-			// {
+			 visited.push_back(u);
+			 //for (int i = 0; i < G->n; i++)		
+			//{
 				 for (int j = 0; j < G->n; j++)
 				 {
-					 if (V->edges[u][j])
+					 if (V->edges[u][j])						// it is a queue so the last element will be display in first 
 					 {
 						 Q.push(j);				
 					 }
@@ -177,14 +153,13 @@ int main()
 	Vertice *V10 = new Vertice(10);
 
 	V1->InitializationOfEdge(); 
-	V2->InitializationOfEdge();
+	/*V2->InitializationOfEdge();
 	V3->InitializationOfEdge();
 	V4->InitializationOfEdge();
 	V5->InitializationOfEdge();
 	V6->InitializationOfEdge();
 	V7->InitializationOfEdge();
-	V8->InitializationOfEdge();
-
+	V8->InitializationOfEdge();*/
 
 	G->addVertex(1);
 	G->addVertex(2);
@@ -196,30 +171,27 @@ int main()
 	G->addVertex(8);
 	G->addVertex(9);
 	G->addVertex(10);
-
-	//Vertice *V1 = new Vertice(1);
 	
-		
-	
-
 	V1->addEdges(V1, V2);
-	
-	V1->addEdges(V2, V3);
+	V1->addEdges(V1, V3);
 	V1->addEdges(V3, V4);
 	V1->addEdges(V3, V6);
-	V1->addEdges(V5, V6);
-	V1->addEdges(V1, V6);
-	V1->addEdges(V4, V7);
-	V1->addEdges(V7, V8);
-	V1->addEdges(V8, V5);
+	V1->addEdges(V6, V7);
+	V1->addEdges(V6, V9);
+	V1->addEdges(V4, V5);
+	V1->addEdges(V4, V8);
+	V1->addEdges(V8, V9);
 
 	list<int> resultBFS = BFS(G, V1);
 	list<int> resultDFS = DFS(G, V1);
 	std::list<int>::const_iterator iterator;
 	
-	/*for (iterator = resultBFS.begin() = resultBFS.begin(); iterator != resultBFS.end(); iterator++)
-		cout << *iterator << " ";*/
+	std::cout << "BFS" << std::endl;
+	for (iterator = resultBFS.begin() = resultBFS.begin(); iterator != resultBFS.end(); iterator++)
+		cout << *iterator << " ";
 		
+	std::cout << "" << std::endl;
+	std::cout << "DFS" << std::endl;
 	for (iterator = resultDFS.begin() = resultDFS.begin(); iterator != resultDFS.end(); iterator++)
 		cout << *iterator << " ";
 		
